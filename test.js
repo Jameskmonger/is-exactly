b36ba15b459d8b87d04cb9d778e8bfcf4e60f402
James Monger <jameskmonger@hotmail.co.uk>
2016-04-04 20:50:12 +0100

    Test for being given the same string twice

--- test.js
+++ test.js
@@ -8,3 +8,9 @@ test('it returns true when given the same number twice', (t, value) => {
 }, [
     [3], [4], [10], [20], [Infinity]
 ]);
+
+test('it returns true when given the same string twice', (t, value) => {
+    t.assert.true(isExactly(value, value));
+}, [
+    ['james'], ['bob'], ['cats'], ['apples'], ['cheese and melon']
+]);
