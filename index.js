21877194e832aa668447cd08ff43faa06c60d933
James Monger <jameskmonger@hotmail.co.uk>
2016-04-04 23:19:03 +0100

    Test keys

--- index.js
+++ index.js
@@ -6,4 +6,20 @@ module.exports = function isExactly (value, other) {
     if (value === other) {
         return true;
     }
+
+    var vKeys = keys(value);
+    var oKeys = keys(other);
+
+    if (vKeys.length !== oKeys.length) {
+        return false;
+    }
+
+    vKeys.sort();
+    oKeys.sort();
+
+    for (var i = 0; i < vKeys.length; i++) {
+        if (vKeys[i] !== oKeys[i]) {
+            return false;
+        }
+    }
 };
